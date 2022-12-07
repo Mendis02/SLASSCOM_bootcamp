5aa218dba7603d0cf3e9db633d8458f4aec03ff9
thanura mendis <thanuramendis02@gmail.com>
2022-12-07 16:56:46 +0530

    made a method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-class Main {
-    public:
-    int romanToInt(string s) {
-        System.out.println("sss");
+class Solution {
+    static int romanToInt(int x) {
+
+        return x;
     }
-};
+    public static void main(String args[]){
+        System.out.println(romanToInt(4));
+
+
+//        Solution ob = new Solution();
+//        ob.romanToInt();
+
+    }
+}
--- src/romantonumber.java
+++ src/romantonumber.java
@@ -1,5 +1,5 @@
-public class romantonumber {
-    public static void main(String args[]){
-        System.out.println('123');
-    }
-}
+//public class romantonumber {
+//    public static void main(String args[]){
+//        System.out.println(12);
+//    }
+//}
