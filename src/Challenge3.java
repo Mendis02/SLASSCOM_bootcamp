0888e6354990649a703ab6e6cd78365b2f0b3290
thanura mendis <thanuramendis02@gmail.com>
2022-12-22 19:07:40 +0530

    finding mode addded. used a hashmap

--- src/Challenge3.java
+++ src/Challenge3.java
@@ -1,44 +1,66 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Challenge3 {
     public static void main(String[] args) {
-        int [] array = {47, 84, 75, 21, 14, 14, 79};
+        int [] array = {47,84,75,21,14,14,79};
         int l = array.length;
         int median;
 
-        //sort the array
-        for (int i = 0; i < l; i++) {
-            for (int j = i + 1; j < l; j++) {
-                int temporary = 0;
-                if (array[i] > array[j]) {
-                    temporary = array[i];   //84
-                    array[i] = array[j];
-                    array[j] = temporary;
+        if (l>0) {
+            //sort the array
+            for (int i = 0; i < l; i++) {
+                for (int j = i + 1; j < l; j++) {
+                    int temporary = 0;
+                    if (array[i] > array[j]) {
+                        temporary = array[i];   //84
+                        array[i] = array[j];
+                        array[j] = temporary;
+                    }
                 }
             }
-        }
-        System.out.println(Arrays.toString(array));
+            System.out.println(Arrays.toString(array));
 
-        //find the median
-        if (l%2 ==0){
-            median = (array[l/2] + array[(l+1)/2])/2;
-        }else{
-            int x = Math.round(l/2);
-            median = array[x];
-        }
-        System.out.println(median);
-
-        //finding the highest number
-        int highest = 0;
-        for(int i = 0; i < l; i++){
-            if (highest<array[i]){
-                highest = array[i];
+            //find the median
+            if (l % 2 == 0) {
+                median = (array[l / 2] + array[(l + 1) / 2]) / 2;
+            } else {
+                int x = Math.round(l / 2);
+                median = array[x];
             }
+            System.out.println("median: " + median);
+
+            //finding the highest number
+            int highest = 0;
+            for (int i = 0; i < l; i++) {
+                if (highest < array[i]) {
+                    highest = array[i];
+                }
+            }
+            System.out.println("highest: " + highest);
+
+            //check mode
+            Map<Integer,Integer> checkMode= new HashMap<Integer,Integer>();
+
+            for (int i=0; i< array.length; i++){
+                if (checkMode.containsKey(array[i])){
+                    checkMode.put(array[i], checkMode.get(array[i])+1);
+                }else{
+                    checkMode.put(array[i], 1);
+                }
+            }
+            int mode = 0;
+            for(Map.Entry<Integer,Integer> entry : checkMode.entrySet()){
+                if(entry.getValue() > mode){
+                    mode = entry.getValue();
+                }
+            }
+            System.out.println("mode: " + mode);
+
+        }else{
+            System.out.println("Array is empty.");
         }
-        System.out.println(highest);
-
-
-
     }
 }
