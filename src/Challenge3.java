23ca67111eeaef5a68ad4610257eb55dd43a5ed7
thanura mendis <thanuramendis02@gmail.com>
2022-12-22 17:11:08 +0530

    array sort, find median and highest number

--- src/Challenge3.java
+++ src/Challenge3.java
@@ -1,10 +1,44 @@
+import java.util.Arrays;
+
+
 public class Challenge3 {
     public static void main(String[] args) {
         int [] array = {47, 84, 75, 21, 14, 14, 79};
-        float median = 0;
+        int l = array.length;
+        int median;
 
-        for(int i=0;i<array.length;i++){
-
+        //sort the array
+        for (int i = 0; i < l; i++) {
+            for (int j = i + 1; j < l; j++) {
+                int temporary = 0;
+                if (array[i] > array[j]) {
+                    temporary = array[i];   //84
+                    array[i] = array[j];
+                    array[j] = temporary;
+                }
+            }
         }
+        System.out.println(Arrays.toString(array));
+
+        //find the median
+        if (l%2 ==0){
+            median = (array[l/2] + array[(l+1)/2])/2;
+        }else{
+            int x = Math.round(l/2);
+            median = array[x];
+        }
+        System.out.println(median);
+
+        //finding the highest number
+        int highest = 0;
+        for(int i = 0; i < l; i++){
+            if (highest<array[i]){
+                highest = array[i];
+            }
+        }
+        System.out.println(highest);
+
+
+
     }
 }
