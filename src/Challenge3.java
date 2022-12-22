8e49ac45ef92c88f7002fa976e17cd8c41231a81
thanura mendis <thanuramendis02@gmail.com>
2022-12-22 16:39:46 +0530

    .

---
+++ src/Challenge3.java
@@ -0,0 +1,10 @@
+public class Challenge3 {
+    public static void main(String[] args) {
+        int [] array = {47, 84, 75, 21, 14, 14, 79};
+        float median = 0;
+
+        for(int i=0;i<array.length;i++){
+
+        }
+    }
+}
